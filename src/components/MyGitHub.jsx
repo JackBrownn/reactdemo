4f7bb3ebc1a120900a82f55d418426e53d5be55b
jack brown <jackbrown@jacks-MacBook-Pro.local>
2022-11-02 14:19:20 -0600

    GitHub link

---
+++ src/components/MyGitHub.jsx
@@ -0,0 +1,8 @@
+const MyGitHub = () => {
+    return (
+        <p>
+            Link to GitHub: <a href="https://github.com/JackBrownn/reactdemo.git"> https://github.com/JackBrownn/reactdemo.git</a>
+        </p>
+    )
+}
+export default MyGitHub;
